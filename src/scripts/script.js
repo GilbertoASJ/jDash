1fa4a81715a61640b0143ba6b0206ca9d63c1a8a
GilbertoASJ <alvesgilberto84@gmail.com>
2021-06-13 17:07:14 -0300

    :sparkles: Creating jDash project

---
+++ src/scripts/script.js
@@ -0,0 +1,11 @@
+// Documentação - 
+$('#documentacao').on('click', () => {
+
+	console.log('Documentação clicado')
+})
+
+// Suporte - 
+$('#suporte').on('click', () => {
+	
+	console.log('suporte clicado')
+})
