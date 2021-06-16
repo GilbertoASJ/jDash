fb0c85dd06201d51f5e4b7b78c7ea0581ea95012
GilbertoASJ <alvesgilberto84@gmail.com>
2021-06-16 16:27:57 -0300

    :sparkles: Updating file

--- src/php/app.php
+++ src/php/app.php
@@ -8,10 +8,12 @@ class Dashboard {
 
 	public $numeroVendas;
 	public $totalVendas;
+	public $clientesAtivos;
+	public $clientesInativos;
 
 	public function __get($attr) {
 
-		return $this->$att;
+		return $this->$attr;
 	}
 
 	public function __set($attr, $value) {
@@ -30,6 +32,7 @@ class Conexao {
 	private $user = 'root';
 	private $password = '';
 
+	// Método de conexão com o banco de dados
 	public function conectar() {
 
 		try {
@@ -64,6 +67,7 @@ class Bd {
 		$this->dashboard = $dashboard;
 	}
 
+	// Função para recuperar o número de vendas
 	public function getNumeroVendas() {
 
 		$query = "SELECT 
@@ -74,23 +78,89 @@ class Bd {
 					data_venda BETWEEN :dataInicio AND :dataFim ";
 
 		$stmt = $this->conexao->prepare($query);
-		$stmt->bindValue(':dataInicio', '2021-02-15');
-		$stmt->bindValue(':dataFim', '2021-04-04');
+		$stmt->bindValue(':dataInicio', $this->dashboard->__get('dataInicio'));
+		$stmt->bindValue(':dataFim', $this->dashboard->__get('dataFim'));
 
 		$stmt->execute();
 
-		return $stmt->fetch(PDO::FETCH_OBJ);
+		return $stmt->fetch(PDO::FETCH_OBJ)->numeroVendas;
+	}
+
+	// Função para recuperar o total de vendas
+	public function getTotalVendas() {
+
+		$query = "SELECT 
+					SUM(total) as totalVendas 
+				FROM 
+					tb_vendas 
+				WHERE 
+					data_venda BETWEEN :dataInicio AND :dataFim ";
+
+		$stmt = $this->conexao->prepare($query);
+		$stmt->bindValue(':dataInicio', $this->dashboard->__get('dataInicio'));
+		$stmt->bindValue(':dataFim', $this->dashboard->__get('dataFim'));
+
+		$stmt->execute();
+
+		return $stmt->fetch(PDO::FETCH_OBJ)->totalVendas;
+	}
+
+	// Função para recuperar o total de clientes ativos
+	public function getClientesAtivos() {
+
+		$query = "SELECT 
+					COUNT(*) as clientesAtivos 
+				FROM 
+					tb_clientes
+				WHERE 
+					cliente_ativo = 1";
+
+		$stmt = $this->conexao->prepare($query);
+
+		$stmt->execute();
+
+		return $stmt->fetch(PDO::FETCH_OBJ)->clientesAtivos;
+	}
+
+	// Função para recuperar o total de clientes inativos
+	public function getClientesInativos() {
+
+		$query = "SELECT 
+					COUNT(*) as clientesInativos 
+				FROM 
+					tb_clientes
+				WHERE 
+					cliente_ativo = 0";
+
+		$stmt = $this->conexao->prepare($query);
+
+		$stmt->execute();
+
+		return $stmt->fetch(PDO::FETCH_OBJ)->clientesInativos;
 	}
 }
 
 // Instânciando as classes
 
+// Dashboard
+
 $dashboard = new Dashboard();
+
+$dashboard->__set('dataInicio', '2021-02-01');
+$dashboard->__set('dataFim', '2021-02-31');
+
+// Conexão
+
 $conexao = new Conexao();
 
+// Bd
 $bd = new Bd($conexao, $dashboard);
-echo "<pre>"; 
-	print_r($bd->getNumeroVendas());
-echo "</pre>";
+
+$dashboard->__set('numeroVendas', $bd->getNumeroVendas());
+$dashboard->__set('totalVendas', $bd->getTotalVendas());
+$dashboard->__set('clientesAtivos', $bd->getClientesAtivos());
+$dashboard->__set('clientesInativos', $bd->getClientesInativos());
+
+print_r($dashboard);
 
 ?>
